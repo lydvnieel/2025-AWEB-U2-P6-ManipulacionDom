24254e56f1e233cdeadcbd454a427e8d230444ba
lydvnieel <20243ds183@utez.edu.mx>
2025-10-15 14:53:38 -0600

    Trabajo 15-10-2025 Calcular area con Js

---
+++ app.js
@@ -0,0 +1,22 @@
+const inputValorCuadrado = document.getElementById('inputValorCuadrado');
+const inputResultado = document.getElementById('inputResultado');
+const btnCalcular = document.getElementById('btnCalcular');
+
+btnCalcular.addEventListener('click', 
+    
+    function() {
+    let valor = parseFloat(inputValorCuadrado.value);
+
+    if (valor <= 0 || valor == "") {
+        inputResultado.textContent = "Error: Tienes que ingresar un número positivo";
+        inputResultado.className = "mensaje-error";
+ 
+    } else {
+        const area = valor * valor;
+        inputResultado.textContent = `El área del cuadrado es: ${area}`;
+        inputResultado.className = ".mensaje-correcto";
+    }
+}
+
+
+);
